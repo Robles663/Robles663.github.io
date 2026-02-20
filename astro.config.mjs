d319f54e2afb2c30b509807ca98cf3f9e1b3a9e5
Alejandro Robles <robleslizarragaa832@gmail.com>
2026-02-20 00:36:22 -0800

    url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://Robles663.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
